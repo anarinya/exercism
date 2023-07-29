18d95de22ce000e131035f2e3728eee972baa8c2
anarinya <5115364+anarinya@users.noreply.github.com>
2023-07-28 21:20:28 -0500

    completed wine-cellar

---
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -0,0 +1,44 @@
+defmodule WineCellar do
+  @type wine :: {variety :: String.t(), year :: integer, country :: String.t()}
+
+  @spec explain_colors() :: [{atom, String.t()}]
+  def explain_colors do
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+  end
+
+  @spec filter(cellar :: [{atom, wine}], color :: atom, opts :: [keyword()]) :: [wine()]
+  def filter(cellar, color, opts \\ []) do
+    cellar
+    |> Keyword.get_values(color)
+    |> filter_by_year(opts[:year])
+    |> filter_by_country(opts[:country])
+  end
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year(wines, nil), do: wines
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country(wines, nil), do: wines
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
