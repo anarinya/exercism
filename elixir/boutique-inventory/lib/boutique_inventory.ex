5772be29e42f0f7d7000b441139a08a199cc70c3
anarinya <5115364+anarinya@users.noreply.github.com>
2023-08-24 21:08:26 -0500

    elixir boutique

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,18 @@
 defmodule BoutiqueInventory do
-  def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
-  end
+  def sort_by_price(inventory), do: Enum.sort_by(inventory, & &1.price)
 
-  def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
-  end
+  def with_missing_price(inventory), do: Enum.filter(inventory, &(!&1.price))
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    Enum.map(inventory, &%{&1 | name: String.replace(&1.name, old_word, new_word)})
   end
 
-  def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+  def increase_quantity(%{quantity_by_size: sizes} = item, count) do
+    new_counts = Enum.flat_map(sizes, &%{elem(&1, 0) => elem(&1, 1) + count})
+    %{item | quantity_by_size: Map.new(new_counts)}
   end
 
-  def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+  def total_quantity(%{quantity_by_size: sizes}) do
+    Enum.reduce(sizes, 0, &(&2 + elem(&1, 1)))
   end
 end
